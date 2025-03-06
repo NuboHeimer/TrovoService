8efa84b7c1651bbb3ead09b23f9c69f4a4c28964
NuboHeimer <NuboHeimer@yandex.ru>
2025-03-06 16:37:33 +0300

    Обновил версию перед полноценным релизом. Добавлен метод сохранения present viewers в список. Добавлен метод сохранения чаттеров в список. Добавлен метод получения случайного зрителя.

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # TrovoService
-Функционал для streamer.bot, который не был реализован разработчиками оного.
+Дополнительный функционал [streamer.bot](https://streamer.bot/), для trovo.
+
+## Disclamer
+PresentViwers для Trovo, так же как и для YouTube не получает список текущих зрителей трансляции (в отличие от твича). Он получает список **активных за выбранный период** зрителей. Так как, по [словам](https://discord.com/channels/834650675224248362/834650675740540941/1339880791991124028) поддержки сб "*trovo's api actually doesn't give user id in their viewers api, sooo there is no way to "link" just the nickname to a user..*".
+
+Соответственно все интерактивы завязанные на список текущих зрителей становятся невозможными. Мой модуль сохраняет список всех зрителей, которые были "пойманы" триггером PresentViewers (%trovoTodayViewers%) и запоминает его до перезапуска бота. Так же он запоминает список всех, кто писал в чат, по триггеру First Words (%trovoTodayChatters%). И так же очищает список при перезапуске бота.
+Кэш First Words надо сбрасывать отдельно.
+
+## Функционал, экшены и их настройка
+При импорте у вас появится группа **\[Services] Trovo**
+- **\[Trovo] Code** -- основной код.
+- **\[Trovo] Get Random Viewers** -- экшен получения случайных зрителей. На данный момент, получает одного зрителя и записывает его ник в аргумент **%randomUserName0%**. Список зрителей составляется из списка чаттеров и списка Present Viewers. Так же случайный зритель попадает в глобальную переменную **trovoLastRandomViewer**. Переменная очищается при закрытии бота. Это сделано для того, чтобы бот не выбирал подряд дважды одного и того же зрителя. Если зритель всего один -- штож, тогда выбирать особо не из кого.
+- **\[Trovo] Save Chatters** -- сохраняет список всех, кто написал в чат, в глобальную переменную **trovoTodayChatters**. Переменная очищается при закрытии бота. Работает по триггеру Trovo -> General -> First Words.
+- **\[Trovo] Save Present Viewers** -- сохраняет список всех, кто попал в триггер Present Viewers, в глобальную переменную **trovoTodayViewers**. Переменная очищается при закрытии бота.
 
 ## License
 This project is licensed under the [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-nc-sa/4.0/).  
--- TrovoService.cs
+++ TrovoService.cs
@@ -4,59 +4,106 @@
 ///   Email:        nuboheimer@yandex.ru
 ///   Help:         https://t.me/nuboheimersb/49
 ///----------------------------------------------------------------------------
-
-///   Version:      0.2.0
+ 
+///   Version:      1.0.0
 using System;
 using System.Collections.Generic;
+
 public class CPHInline
 {
-
     public bool SavePresentViewers()
     {
-
         if (!args.ContainsKey("users")) // защита от дурака с пустым аргументом списка пользователей.
+        {
             return false;
+        }
 
         var currentViewers = (List<Dictionary<string, object>>)args["users"]; // запоминаем текущий список зрителей из аргумента бота.
 
         if (currentViewers.Count == 0)
-            return true; // выходим, если список зрителей всё же пустой.
+        {
+            return false; // выходим, если список зрителей всё же пустой.
+        }
 
-        List<string> trovoPresentViewers = new List<string>(); // инициализируем переменную для списка ников зрителей.
+        List<string> trovoTodayViewers = new List<string>(); // инициализируем переменную для списка "сегодняшних" зрителей.
+
+        if (CPH.GetGlobalVar<List<string>>("trovoTodayViewers", false) != null) // если есть сохранённый ранее список записываем его.
+        {
+            trovoTodayViewers = CPH.GetGlobalVar<List<string>>("trovoTodayViewers", false);
+        }
 
         foreach (var viewer in currentViewers) // проходимся по зрителям в списке.
         {
-            trovoPresentViewers.Add(viewer["userName"].ToString());
+            string userName = viewer["userName"].ToString();
+            if (!trovoTodayViewers.Contains(userName))
+                trovoTodayViewers.Add(userName);
         }
 
-        CPH.SetGlobalVar("trovoPresentViewers", trovoPresentViewers, false);
+        CPH.SetGlobalVar("trovoTodayViewers", trovoTodayViewers, false);
+        return true;
+    }
 
+    public bool SaveChatters()
+    {
+        CPH.TryGetArg("userName", out string userName);
+        List<string> trovoTodayChatters = new List<string>(); // инициализируем переменную для списка ников зрителей.
+
+        if (CPH.GetGlobalVar<List<string>>("trovoTodayChatters", false) != null) // если есть сохранённый ранее список записываем его.
+        {
+            trovoTodayChatters = CPH.GetGlobalVar<List<string>>("trovoTodayChatters", false);
+        }
+
+        if (!trovoTodayChatters.Contains(userName))
+        {
+            trovoTodayChatters.Add(userName);
+        }
+
+        CPH.SetGlobalVar("trovoTodayChatters", trovoTodayChatters, false);
         return true;
     }
 
     public bool GetRandomViewers()
     {
+        List<string> viewers = new List<string>();
 
-        if (CPH.GetGlobalVar<List<string>>("trovoPresentViewers", false) == null)
-            return false;
-
-        List<string> trovoPresentViewers = CPH.GetGlobalVar<List<string>>("trovoPresentViewers", true);
-
-        if (!CPH.TryGetArg("usersCount", out int usersCount))
-            usersCount = 1;
-
-        if (usersCount > 1)
-            if (usersCount > trovoPresentViewers.Count)
-                usersCount = trovoPresentViewers.Count;
-
-        for (int i = 0; i < usersCount; i++)
+        if (CPH.GetGlobalVar<List<string>>("trovoTodayViewers", false) != null)
         {
-            Random random = new Random();
-            string userName = trovoPresentViewers[random.Next(trovoPresentViewers.Count)].ToString();
-            trovoPresentViewers.Remove(userName);
-            CPH.SetArgument($"userName{i}", userName);
+            viewers.AddRange(CPH.GetGlobalVar<List<string>>("trovoTodayViewers", false));
         }
 
+        if (CPH.GetGlobalVar<List<string>>("trovoTodayChatters", false) != null)
+        {
+            foreach (string viewer in CPH.GetGlobalVar<List<string>>("trovoTodayChatters", false))
+            {
+                {
+                    {
+                        if (!viewers.Contains(viewer))
+                            viewers.Add(viewer);
+                    }
+                }
+            }
+        }
+
+        if (viewers.Count == 0)
+        {
+            CPH.LogError($"Списки зрителей отсутствуют!");
+            return false;
+        }
+
+        Random random = new Random();
+        string randomUserName = viewers[random.Next(viewers.Count)];
+        
+        if (viewers.Count > 1)
+        {
+            while (randomUserName.Equals(CPH.GetGlobalVar<string>("trovoLastRandomViewer", false)))
+            {
+                viewers.Remove(randomUserName);
+                randomUserName = viewers[random.Next(viewers.Count)];
+            }
+        }
+
+        CPH.SetArgument($"randomUserName0", randomUserName);
+        CPH.SetGlobalVar("trovoLastRandomViewer", randomUserName, false);
         return true;
     }
 }
